37e64fa87885f34f8c54de0a1a64650d734818be
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-11-25 14:53:05 +0300

    3.5 - 5

---
+++ 3. Постижение строк и списков/3.5 Форматирование строк и F-строки/5.py
@@ -0,0 +1,4 @@
+dollar_exchange_rate = float(input())
+number_of_rubles = int(input())
+
+print(f'Вы можете получить {int(number_of_rubles / dollar_exchange_rate)}$ за {number_of_rubles} рублей по курсу {dollar_exchange_rate}')

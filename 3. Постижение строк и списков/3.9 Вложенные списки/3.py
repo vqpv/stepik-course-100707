23ee3dbcab89379b8c98f77e4b6baa536fd17ad8
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-12-15 21:20:22 +0300

    3.9 - 3

---
+++ 3. Постижение строк и списков/3.9 Вложенные списки/3.py
@@ -0,0 +1,7 @@
+matrix = []
+
+matrix.append(list(map(int, input().split())))
+matrix.append(list(map(int, input().split())))
+matrix.append(list(map(int, input().split())))
+
+print(matrix[0][-1], matrix[1][-1], matrix[2][-1])

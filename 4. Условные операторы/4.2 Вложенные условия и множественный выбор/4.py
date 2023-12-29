4083c00c02402d60791c09b8ddcb0ff612d29092
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-12-29 22:20:32 +0300

    4.2 - 4

---
+++ 4. Условные операторы/4.2 Вложенные условия и множественный выбор/4.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num == 1:
+    print("понедельник")
+elif num == 2:
+    print("вторник")
+elif num == 3:
+    print("среда")
+elif num == 4:
+    print("четверг")
+elif num == 5:
+    print("пятница")
+elif num == 6:
+    print("суббота")
+elif num == 7:
+    print("воскресенье")

cd6d9307fc3e98e38f550095e7e7cd7b7c39e3f7
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-12-30 21:45:06 +0300

    4.2 - 7

---
+++ 4. Условные операторы/4.2 Вложенные условия и множественный выбор/7.py
@@ -0,0 +1,16 @@
+d = int(input())
+
+if d % 7 == 1:
+    print("понедельник")
+elif d % 7 == 2:
+    print("вторник")
+elif d % 7 == 3:
+    print("среда")
+elif d % 7 == 4:
+    print("четверг")
+elif d % 7 == 5:
+    print("пятница")
+elif d % 7 == 6:
+    print("суббота")
+elif d % 7 == 0:
+    print("воскресенье")
